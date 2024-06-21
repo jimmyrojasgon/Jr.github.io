2638f4f710d2c8201c4af02c40aa51e4ba92858a
jimmyrojasgon <77075586@certus.edu.pe>
2024-06-20 21:17:50 -0500

    mejoras

--- js/login.js
+++ js/login.js
@@ -1,15 +1,26 @@
-$( ".input" ).focusin(function() {
-    $( this ).find( "span" ).animate({"opacity":"0"}, 200);
-  });
-  
-  $( ".input" ).focusout(function() {
-    $( this ).find( "span" ).animate({"opacity":"1"}, 300);
-  });
-  
-  $(".login").submit(function(){
-    $(this).find(".submit i").removeAttr('class').addClass("fa fa-check").css({"color":"#fff"});
-    $(".submit").css({"background":"#2ecc71", "border-color":"#2ecc71"});
-    $(".feedback").show().animate({"opacity":"1", "bottom":"-80px"}, 400);
-    $("input").css({"border-color":"#2ecc71"});
-    return false;
-  });
+
+  const email = document.getElementById("email")
+  const pass = document.getElementById("pass")
+  const parrafo = document.getElementById('warnings')
+  const form = document.getElementById('formulario')
+    
+    form.addEventListener('button', e=>{
+      e.preventDefault();
+      let warnings = " "
+      let entrar = false
+      let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+      parrafo.innerHTML = " "
+      console.log(regexEmail.test(email.value))
+      if(!regexEmail.test(email.value)){
+        warnings += 'El email no es valido <br>' 
+        entrar = true
+      }
+      if(pass.value.length <8){
+        warnings += 'La contraseña no es valido <br>'
+        entrar = true
+      }
+      if(entrar){
+        parrafo.innerHTML = warnings
+      }
+
+    })
