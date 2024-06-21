5f1b1094c2989845ce383937f17476dc73f6cc1f
jimmyrojasgon <77075586@certus.edu.pe>
2024-06-21 18:36:53 -0500

    update

--- js/login.js
+++ js/login.js
@@ -1,26 +1,29 @@
 
   const email = document.getElementById("email")
   const pass = document.getElementById("pass")
-  const parrafo = document.getElementById('warnings')
-  const form = document.getElementById('formulario')
-    
-    form.addEventListener('button', e=>{
-      e.preventDefault();
+  const parrafo = document.getElementById("warnings")
+  const form = document.getElementById("form")
+
+    form.addEventListener("submit", e=>{
+      e.preventDefault()
       let warnings = " "
       let entrar = false
-      let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+      let regexEmail =/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
       parrafo.innerHTML = " "
       console.log(regexEmail.test(email.value))
       if(!regexEmail.test(email.value)){
         warnings += 'El email no es valido <br>' 
-        entrar = true
+        entrar = false
       }
       if(pass.value.length <8){
         warnings += 'La contraseña no es valido <br>'
-        entrar = true
+        entrar = false
       }
-      if(entrar){
-        parrafo.innerHTML = warnings
+      if(!entrar){
+        parrafo.innerHTML = warnings;
       }
 
     })
+
+    
+    
